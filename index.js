00f98e6d2b1afd6753573181315ab26dd73c2758
Shivam Gupta <72003102+shivamgupta88@users.noreply.github.com>
2021-07-20 20:35:07 +0530

    first-commit
    done

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomDiceImage1 = "dice"+randomNumber1 + ".png";
+var randomImageSource1 = "images/"+randomDiceImage1 ;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSource1);  
+
+// 2
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var randomDiceImage2 = "dice"+randomNumber2 + ".png";
+var randomImageSource2 = "images/"+randomDiceImage2 ;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSource2);  
+
+// Header
+
+    if(randomNumber1>randomNumber2){
+        document.getElementById("winner").innerHTML= "Player1 wins";    
+    }else if(randomNumber1<randomNumber2){
+        document.getElementById("winner").innerHTML= "Player2 wins";        
+    }else{
+        document.getElementById("winner").innerHTML= "Draw refresh again";        
+
+    }
